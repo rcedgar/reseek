23402bb644eb09a469639a40fa59eaa404661c0d
Robert Edgar <robert@drive5.com>
2024-06-12 07:02:19 -0700

    Ongoing work trouble-shooting build_linux.py

--- src/build_linux.py
+++ src/build_linux.py
@@ -8,7 +8,7 @@ if rc != 0:
     sys.stderr.write("\n\nERROR -- Uncommited changes\n\n")
     sys.exit(1)
 
-rc = os.system('git log --oneline | head -n1 | cut "-d " -f1 | tee gitver.txt')
+rc = os.system('echo \"$(git log --oneline | head -n1 | cut "-d " -f1)\" | tee gitver.txt')
 if rc != 0:
     sys.stderr.write("\n\nERROR -- failed to generate gitver.txt\n\n")
     sys.exit(1)
