f4a9b9e133cce4d734f8cb57295a1d4f32e09b76
Robert Edgar <robert@drive5.com>
2024-06-13 10:02:54 -0700

    Add -output option to calibrate2 to report TS vs. P scatterplot

--- src/calibrate2.cpp
+++ src/calibrate2.cpp
@@ -33,4 +33,36 @@ void cmd_calibrate2()
 	SB.SetTFs();
 	const float MaxFPR = 0.1;
 	SB.SetStats(MaxFPR, true);
+	//ROCStepsToTsv(opt_roc, m_SmoothScores, m_SmoothNTPs, m_SmoothNFPs,
+	//  m_SmoothTPRs, m_SmoothFPRs);
+	const vector<float> &Scores = SB.m_SmoothScores;
+	const vector<uint> &NTPs = SB.m_SmoothNTPs;
+	const vector<uint> &NFPs = SB.m_SmoothNFPs;
+	const vector<float> &TPRs = SB.m_SmoothTPRs;
+	const vector<float> &FPRs = SB.m_SmoothFPRs;
+	const uint N = SIZE(Scores);
+	asserta(SIZE(TPRs) == N);
+	asserta(SIZE(FPRs) == N);
+	asserta(SIZE(NTPs) == N);
+	asserta(SIZE(NFPs) == N);
+	uint NQ = SB.m_QueryChainCount;
+	uint TotalAlns = NQ*NQ;
+
+	if (optset_output)
+		{
+		FILE *f = CreateStdioFile(opt_output);
+		fprintf(f, "TS\tP\n");
+		for (uint i = 0; i < N; ++i)
+			{
+			float TS = Scores[i];
+			uint NFP = NFPs[i];
+			if (NFP < NQ/10)
+				continue;
+			if (NFP > NQ*10)
+				break;
+		// P is probability hit is FP if TS >= thisTS
+			double P = double(NFP)/TotalAlns;
+			fprintf(f, "%.4g\t%.4g\n", TS, P);
+			}
+		}
 	}
