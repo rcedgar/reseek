7e7be2b02608488f2f1ab5e5adb874748c395f3b
Robert Edgar <robert@drive5.com>
2024-06-30 17:56:16 -0700

    Update build_win.bash to repeat uncommited errors warning when finished

--- src/build_win.bash
+++ src/build_win.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+warn=0
 test -z $(git status --porcelain) 2> /dev/null
 if [ $? != 0 ] ; then
+	warn=1
 	echo
 	echo "WARNING -- uncommitted changes"
 	echo
@@ -10,3 +12,9 @@ fi
 echo \"$(git log --oneline | head -n1 | cut "-d " -f1)\" | tee gitver.txt
 
 MSBuild.exe -property:Configuration=Release | tee MSBuild.log
+
+if [ $warn == 1 ] ; then
+	echo
+	echo "WARNING -- uncommitted changes"
+	echo
+fi
