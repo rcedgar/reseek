93c53a51c4c649896e7e16bdb66fc22a2368a430
Robert Edgar <robert@drive5.com>
2024-08-22 07:07:08 -0700

    Modify github workflows to call build_os.bash for each o/s

--- src/build_osx_x64.bash
+++ src/build_osx_x64.bash
@@ -1,3 +1,3 @@
 curl -fsSL https://raw.githubusercontent.com/rcedgar/vcxproj_make/806d016/vcxproj_make.py \
   > vcxproj_make.py
-python3 ./vcxproj_make.py --openmp
+python3 ./vcxproj_make.py --openmp --cppcompiler g++-11 --ccompiler gcc-11
