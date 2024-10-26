d614b598c16bd20dc06a2b24e8415704e5a6b268
Robert Edgar <robert@drive5.com>
2024-10-25 18:00:36 -0700

    Add mkdir ../bin to build*.bash

--- src/build_linux_x86.bash
+++ src/build_linux_x86.bash
@@ -1,4 +1,6 @@
 curl -fsSL https://raw.githubusercontent.com/rcedgar/vcxproj_make/806d016/vcxproj_make.py \
   > vcxproj_make.py
 
+mkdir -p ../bin
+
 python3 ./vcxproj_make.py --openmp
--- src/build_osx_x86.bash
+++ src/build_osx_x86.bash
@@ -1,3 +1,6 @@
 curl -fsSL https://raw.githubusercontent.com/rcedgar/vcxproj_make/806d016/vcxproj_make.py \
   > vcxproj_make.py
+
+mkdir -p ../bin
+
 python3 ./vcxproj_make.py --openmp --cppcompiler g++-11 --ccompiler gcc-11
