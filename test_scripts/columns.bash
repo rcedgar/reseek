25a1340fc3f4e5ad31d62796fc9e6eeac723c465
Robert Edgar <robert@drive5.com>
2024-10-28 07:31:34 -0700

    Fix columns.bash test script to match new default qual+query+target+evalue

--- test_scripts/columns.bash
+++ test_scripts/columns.bash
@@ -11,7 +11,7 @@ $reseek \
 $reseek \
 	-search dir.bca \
 	-fast \
-	-columns qual+query+target_evalue \
+	-columns qual+query+target+evalue \
 	-output columns_same_as_default.tsv \
 	-log columns_same_as_default.log
 
