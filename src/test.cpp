641cf733b6847581bbba81f6a8882b5a55ee9ceb
Robert Edgar <robert@drive5.com>
2024-11-03 13:32:26 -0800

    Checkpoint for debugging memory blowup with db load, BodyVec does blow up

--- src/test.cpp
+++ src/test.cpp
@@ -2,7 +2,6 @@
 #include "chainreader2.h"
 #include "dss.h"
 #include "dssaligner.h"
-#include "profileloader.h"
 
 float GetSelfRevScore(DSSAligner &DA, DSS &D, 
   const PDBChain &Chain, const vector<vector<byte> > &Profile);
@@ -11,7 +10,52 @@ static ChainReader2 CR;
 static DSSParams Params;
 static uint g_Count = 0;
 
-static void Test2ThreadBody(uint ThreadIndex)
+static void BodyVec()
+	{
+	DSSParams DA_Params = Params;
+	DA_Params.m_UsePara = false;
+	DA_Params.m_Omega = 0;
+
+	vector<DSS *> Ds;
+	vector<DSSAligner *> DAs;
+	for (uint i = 0; i < 32; ++i)
+		{
+		DSS *ptrD = new DSS;
+		DSSAligner *ptrDA = new DSSAligner;
+		ptrD->SetParams(Params);
+		ptrDA->m_Params = &DA_Params;
+		Ds.push_back(ptrD);
+		DAs.push_back(ptrDA);
+		}
+
+	time_t Last = time(0);
+	uint Counter = 0;
+	for (;;)
+		{
+		++g_Count;
+		PDBChain *Chain = CR.GetNext();
+		if (Chain == 0)
+			break;
+		time_t Now = time(0);
+		if (Now - Last > 0)
+			{
+			Last = Now;
+			static mutex m;
+			m.lock();
+			Progress("Testing %s %s\r", IntToStr(g_Count),
+					MemBytesToStr(GetMemUseBytes()));
+			m.unlock();
+			}
+		vector<vector<byte> > Profile;
+		DSS &D = *Ds[++Counter%32];
+		DSSAligner &DA = *DAs[++Counter%32];
+		D.Init(*Chain);
+		D.GetProfile(Profile);
+		float SelfRevScore = GetSelfRevScore(DA, D, *Chain, Profile);
+		}
+	}
+
+static void ThreadBody(uint ThreadIndex)
 	{
 	DSS D;
 	D.SetParams(Params);
@@ -46,16 +90,18 @@ static void Test2ThreadBody(uint ThreadIndex)
 		}
 	}
 
-static void Test2()
+void cmd_test()
 	{
 	CR.Open(g_Arg1);
 	Params.SetFromCmdLine(10000);
+	BodyVec();
+	return;
 	const uint ThreadCount = GetRequestedThreadCount();
 
 	vector<thread *> ts;
 	for (uint ThreadIndex = 0; ThreadIndex < ThreadCount; ++ThreadIndex)
 		{
-		thread *t = new thread(Test2ThreadBody, ThreadIndex);
+		thread *t = new thread(ThreadBody, ThreadIndex);
 		ts.push_back(t);
 		}
 	for (uint ThreadIndex = 0; ThreadIndex < ThreadCount; ++ThreadIndex)
@@ -63,50 +109,4 @@ static void Test2()
 
 	for (uint ThreadIndex = 0; ThreadIndex < ThreadCount; ++ThreadIndex)
 		delete ts[ThreadIndex];
-	}
-
-static void Test1()
-	{
-	ProfileLoader PL;
-	ChainReader2 CR;
-	CR.Open(g_Arg1);
-
-	DSSParams Params;
-	Params.SetFromCmdLine(10000);
-
-	static vector<vector<byte> *> m_ComboLettersVec;
-	static vector<vector<uint> *> m_KmerBitsVec;
-	static vector<PDBChain *> m_DBChains;
-	static vector<vector<vector<byte> > *> m_Profiles;
-	static vector<float> m_DBSelfRevScores;
-
-	uint ThreadCount = GetRequestedThreadCount();
-
-	vector<vector<byte> *> *ptrComboLetters = &m_ComboLettersVec;
-	vector<vector<uint> *> *ptrKmerBitsVec = &m_KmerBitsVec;
-	vector<float> *ptrSelfRevScores = &m_DBSelfRevScores;
-	//ptrSelfRevScores = 0;
-
-	if (Params.m_MinU <= 0)
-		ptrKmerBitsVec = 0;
-	if (Params.m_Omega <= 0)
-		ptrComboLetters = 0;
-
-	ProgressLogPrefix("Before Load %.3g cap=%.3g\n",
-	  GetMemUseBytes(), double(m_DBSelfRevScores.capacity()));
-	PL.Load(Params, CR, &m_DBChains, &m_Profiles, ptrComboLetters,
-	  ptrKmerBitsVec, ptrSelfRevScores, ThreadCount);
-	ProgressLogPrefix("Before return %.3g cap=%.3g\n",
-	  GetMemUseBytes(), double(m_DBSelfRevScores.capacity()));
-	}
-
-void cmd_test()
-	{
-	switch (opt_n)
-		{
-	case 1:	Test1(); break;
-	case 2:	Test2(); break;
-	default: asserta(false);
-		}
-	ProgressLogPrefix("After return %.3g\n", GetMemUseBytes());
 	}
