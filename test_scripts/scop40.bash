a136f5b61075bb9b192b5764712e57fe477a965c
Robert Edgar <robert@drive5.com>
2024-12-29 09:39:06 -0800

    Add -dbsize to scop40 test scripts

--- test_scripts/scop40.bash
+++ test_scripts/scop40.bash
@@ -6,12 +6,14 @@ $reseek \
 	-search scop40.bca \
 	-output scop40-fast.tsv \
 	-fast \
+	-dbsize 11206 \
 	-log scop40-fast.log
 
 $reseek \
 	-search scop40.bca \
 	-output scop40-sensitive.tsv \
 	-sensitive \
+	-dbsize 11206 \
 	-log scop40-sensitive.log
 
 $reseek \
@@ -19,4 +21,5 @@ $reseek \
 	-output scop40-evalue1.tsv \
 	-fast \
 	-evalue 1 \
+	-dbsize 11206 \
 	-log scop40-evalue1.log
