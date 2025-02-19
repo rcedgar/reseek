5076ff1c9621873d89cc90330b1c1e3fef6b6009
Robert Edgar <robert@drive5.com>
2025-02-19 10:20:08 -0800

    SEPQ0.1=0.2162 SEPQ1=0.3405 SEPQ10=0.4583 Area=0.8379 02:21

--- src/prefiltermuparams.h
+++ src/prefiltermuparams.h
@@ -1,28 +1,13 @@
 #pragma once
 
-// pattern=1101011
+// pattern=1110011
 static const uint k = 5;
 static const uint K = 7;
-static const uint8_t s_Offsets[] = { 0, 1, 3, 5, 6 };
-
-//// pattern=11111
-//static const uint k = 5;
-//static const uint K = 5;
-//static const uint8_t s_Offsets[] = { 0, 1, 2, 3, 4 };
-
-//// pattern=101010101
-//static const uint k = 5;
-//static const uint K = 9;
-//static const uint8_t s_Offsets[] = { 0, 2, 4, 6, 8 };
-
-//// pattern=111011
-//static const uint k = 5;
-//static const uint K = 6;
-//static const uint8_t s_Offsets[] = { 0, 1, 2, 4, 5 };
+static const uint8_t s_Offsets[] = { 0, 1, 2, 5, 6 };
 
 static const uint8_t *s_ptrOffsets = s_Offsets;
 static const uint DICT_SIZE = 60466176;	// 36^5
-static const uint RSB_SIZE = 1000;
+static const uint RSB_SIZE = 2000;
 static const uint ALPHABET_SIZE = 36;
 
 static const int MIN_KMER_PAIR_SCORE = 32;
