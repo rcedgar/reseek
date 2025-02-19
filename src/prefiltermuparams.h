31a732eae16af33ff1d8ed3e475ad498da11d49c
Robert Edgar <robert@drive5.com>
2025-02-19 10:06:04 -0800

    SEPQ0.1=0.2168 SEPQ1=0.3372 SEPQ10=0.4413 Area=0.8175 01:33

--- src/prefiltermuparams.h
+++ src/prefiltermuparams.h
@@ -1,17 +1,31 @@
 #pragma once
 
 // pattern=1101011
-
 static const uint k = 5;
 static const uint K = 7;
 static const uint8_t s_Offsets[] = { 0, 1, 3, 5, 6 };
+
+//// pattern=11111
+//static const uint k = 5;
+//static const uint K = 5;
+//static const uint8_t s_Offsets[] = { 0, 1, 2, 3, 4 };
+
+//// pattern=101010101
+//static const uint k = 5;
+//static const uint K = 9;
+//static const uint8_t s_Offsets[] = { 0, 2, 4, 6, 8 };
+
+//// pattern=111011
+//static const uint k = 5;
+//static const uint K = 6;
+//static const uint8_t s_Offsets[] = { 0, 1, 2, 4, 5 };
+
 static const uint8_t *s_ptrOffsets = s_Offsets;
 static const uint DICT_SIZE = 60466176;	// 36^5
-static const uint RSB_SIZE = 500;
+static const uint RSB_SIZE = 1000;
 static const uint ALPHABET_SIZE = 36;
 
-// N=60466176, Min=20, LoQ=43, Med=47, HiQ=51, Max=75, Avg=47.3611
-static const int MIN_KMER_PAIR_SCORE = 25;
+static const int MIN_KMER_PAIR_SCORE = 32;
 
 // Not used
 static const int BIAS_WINDOW = 30; // 40
