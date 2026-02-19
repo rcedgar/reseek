8947c123e78c859021e5468f3f9a8b18e73d7c38
Robert Edgar <robert@drive5.com>
2026-02-19 10:36:05 -0800

    Update gitver.bat still trying to avoid loops

--- src/gitver.bat
+++ src/gitver.bat
@@ -1,1 +1,11 @@
-c:\cygwin64\bin\bash -c ./gitver.bash
+@echo off
+setlocal
+
+REM %~dp0 is this script's directory (Windows path, ends with backslash)
+REM ProjectDir points to the .vcxproj directory; use that as the base.
+set "PROJDIR=%~1"
+if "%PROJDIR%"=="" set "PROJDIR=%CD%"
+
+REM Convert Windows path to WSL path and run the script
+wsl bash -lc "cd \"$(wslpath -a '%PROJDIR%')\" && ./gitver.bash"
+exit /b %ERRORLEVEL%
