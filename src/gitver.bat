defa296b4810849973711fc32e583faa70aff4c9
Robert Edgar <robert@drive5.com>
2026-02-19 10:13:42 -0800

    Update gitver.bat still trying to avoid loops

--- src/gitver.bat
+++ src/gitver.bat
@@ -1,16 +1,1 @@
-@echo off
-
-if exist gitver.txt (
-	echo gitver.txt found
-) else (
-	echo "-" > gitver.txt
-)
-
-if exist c:\cygwin64\bin\bash.exe (
-	echo bash found
-	c:\cygwin64\bin\bash -c ./gitver.bash
-) else (
-	echo bash not found
-	echo 000 > gitver.txt
-	exit
-)
+c:\cygwin64\bin\bash -c ./gitver.bash
