54b964dcae853bc697fef7d7aa9359b06c1b8dfd
Robert Edgar <robert@drive5.com>
2026-02-19 10:38:14 -0800

    Update gitver.bash still trying to avoid loops

--- src/gitver.bash
+++ src/gitver.bash
@@ -9,14 +9,6 @@ if [ ! -d ../.git ] ; then
 fi
 
 PATH=$PATH:/usr/bin
-
-hold=`cat git_hash.h`
 h=`git describe --abbrev=7 --dirty --long --always`
-h=$h
-hold=$hold
-if [[ $h == $hold ]] ; then
-    echo Same version githash=$h
-else
-    echo New version githash=$h
-    echo "#define GIT_HASH \"$h\"" > git_hash.h
-fi
+echo $h
+echo "#define GIT_HASH \"$h\"" > git_hash.h
