e539e90514e00f69cfc9eea7079c37424197b725
Robert Edgar <robert@drive5.com>
2026-03-03 16:42:40 -0800

    distmx_kernel.cpp cmd_test_dist_mx() pass scop40_onek test for neighbors and distances

--- src/distmx_kernel.cpp
+++ src/distmx_kernel.cpp
@@ -85,12 +85,69 @@ static void SimpleTest()
 		}
 	}
 
+static void GetNNs(const PDBChain &Chain, uint i, uint16_t &men, uint16_t &pen)
+	{
+	men = UINT16_MAX;
+	pen = UINT16_MAX;
+	const uint L = Chain.GetSeqLength();
+	float mmind = FLT_MAX;
+	float pmind = FLT_MAX;
+	for (uint j = 0; j < L; ++j)
+		{
+		if (abs(int(i)-int(j)) > distmx::M)
+			continue;
+		if (abs(int(i)-int(j)) <= distmx::m_skip)
+			continue;
+		float d = Chain.GetDist(i, j);
+		if (j < i && d < mmind)
+			{
+			men = j;
+			mmind = d;
+			}
+		if (j > i && d < pmind)
+			{
+			pen = j;
+			pmind = d;
+			}
+		}
+	}
+
+static float GetDist(const PDBChain &Chain, uint i, uint j)
+	{
+	const uint L = Chain.GetSeqLength();
+	if (i >= L || j >= L)
+		return -1;
+	return Chain.GetDist(i, j);
+	}
+
 static void compare_kernel(const PDBChain &Chain, const distmx::Out out,
-	float t, uint64 &ntest, uint32_t &nfail)
+	float t, uint64 &ndtest, uint32_t &ndfail, uint32_t &nntest, uint32_t &nnfail)
 	{
 	const uint L = Chain.GetSeqLength();
 	for (uint i = 0; i < L; ++i)
 		{
+		uint16_t men, pen;
+		GetNNs(Chain, i, men, pen);
+		uint16_t men_kernel = out.prev[i];
+		uint16_t pen_kernel = out.next[i];
+		++nntest;
+		if (men_kernel != men) 
+			{
+			++nnfail;
+			Log("[%4u]  %4u(%7.3g) %4u(%7.3g) MEN diff\n",
+				i,
+				men, GetDist(Chain, i, men),
+				men_kernel, GetDist(Chain, i, men_kernel));
+			}
+		if (pen_kernel != pen)
+			{
+			++nnfail;
+			Log("[%4u]  %4u(%7.3g) %4u(%7.3g) PEN diff\n",
+				i,
+				pen, GetDist(Chain, i, pen),
+				pen_kernel, GetDist(Chain, i, pen_kernel));
+			}
+
 		for (uint j = 0; j < i; ++j)
 			{
 			if (abs(int(i)-int(j)) >= distmx::M)
@@ -100,9 +157,9 @@ static void compare_kernel(const PDBChain &Chain, const distmx::Out out,
 			assert(k < K);
 			uint16_t dist_code_kernel = out.dist[k];
 			float d_kernel = distmx::decode_code_to_distance(dist_code_kernel);
-			++ntest;
+			++ndtest;
 			if (fabs(d_kernel - d) > t)
-				++nfail;
+				++ndfail;
 			}
 		}
 	}
@@ -113,22 +170,27 @@ void cmd_test_dist_mx()
 #if 0
 	SimpleTest();
 	return;
-#endif
-#if 1
+#else
 	vector<PDBChain *> Chains;
 	ReadChains(g_Arg1, Chains);
 	FILE *f = CreateStdioFile(opt(output));
 	const uint ChainCount = SIZE(Chains);
 	DSS D;
-	uint64 ntest_ref = 0;
-	uint64 ntest_avx2 = 0;
-	uint64 ntest_avx5 = 0;
-	uint32 nfail_ref = 0;
-	uint32 nfail_avx2 = 0;
-	uint32 nfail_avx5 = 0;
+	uint64 ndtest_ref = 0;
+	uint64 ndtest_avx2 = 0;
+	uint64 ndtest_avx5 = 0;
+	uint32 ndfail_ref = 0;
+	uint32 ndfail_avx2 = 0;
+	uint32 ndfail_avx5 = 0;
+	uint32 nntest_ref = 0;
+	uint32 nnfail_ref = 0;
+	uint32 nntest_avx2 = 0;
+	uint32 nnfail_avx2 = 0;
+	uint32 nntest_avx5 = 0;
+	uint32 nnfail_avx5 = 0;
 	for (uint ChainIdx = 0; ChainIdx < ChainCount; ++ChainIdx)
 		{
-		ProgressStep(ChainIdx, ChainCount, "Testing nfail_ref=%u", nfail_ref);
+		ProgressStep(ChainIdx, ChainCount, "Testing nnfail_ref=%u", nnfail_ref);
 		const PDBChain &Chain = *Chains[ChainIdx];
 		D.Init(Chain);
 
@@ -146,23 +208,30 @@ void cmd_test_dist_mx()
 		const float maxdiff = 0.01;
 
 		distmx::kernel_scalar_ref(xic.data(), yic.data(), zic.data(), L, out);
-		compare_kernel(Chain, out, maxdiff, ntest_ref, nfail_ref);
+		compare_kernel(Chain, out, maxdiff, ndtest_ref, ndfail_ref, nntest_ref, nnfail_ref);
 
 		distmx::kernel_avx2(xic.data(), yic.data(), zic.data(), L, out);
-		compare_kernel(Chain, out, maxdiff, ntest_avx2, nfail_avx2);
+		compare_kernel(Chain, out, maxdiff, ndtest_avx2, ndfail_avx2, nntest_avx2, nnfail_avx2);
 
 		distmx::kernel_avx512bw(xic.data(), yic.data(), zic.data(), L, out);
-		compare_kernel(Chain, out, maxdiff, ntest_avx5, nfail_avx5);
+		compare_kernel(Chain, out, maxdiff, ndtest_avx5, ndfail_avx5, nntest_avx5, nnfail_avx5);
 
 		free_out(out);
 		}
 	_chkmem();
 	CloseStdioFile(f);
-	ProgressLog("%u fail / %s (%.3g%%)\n", 
-		nfail_ref, Int64ToStr(ntest_ref), GetPct(nfail_ref, double(ntest_ref)));
-	ProgressLog("%u fail / %s (%.3g%%)\n",
-		nfail_avx2, Int64ToStr(ntest_avx2), GetPct(nfail_avx2, double(ntest_avx2)));
-	ProgressLog("%u fail / %s (%.3g%%)\n",
-		nfail_avx5, Int64ToStr(ntest_avx5), GetPct(nfail_avx5, double(ntest_avx5)));
+	ProgressLog("%u dist fail / %s (%.3g%%)\n", 
+		ndfail_ref, Int64ToStr(ndtest_ref), GetPct(ndfail_ref, double(ndtest_ref)));
+	ProgressLog("%u dist fail / %s (%.3g%%)\n",
+		ndfail_avx2, Int64ToStr(ndtest_avx2), GetPct(ndfail_avx2, double(ndtest_avx2)));
+	ProgressLog("%u dist fail / %s (%.3g%%)\n",
+		ndfail_avx5, Int64ToStr(ndtest_avx5), GetPct(ndfail_avx5, double(ndtest_avx5)));
+
+	ProgressLog("%u nn fail / %s (%.3g%%)\n", 
+		nnfail_ref, Int64ToStr(nntest_ref), GetPct(nnfail_ref, double(nntest_ref)));
+	ProgressLog("%u nn fail / %s (%.3g%%)\n",
+		nnfail_avx2, Int64ToStr(nntest_avx2), GetPct(nnfail_avx2, double(nntest_avx2)));
+	ProgressLog("%u nn fail / %s (%.3g%%)\n",
+		nnfail_avx5, Int64ToStr(nntest_avx5), GetPct(nnfail_avx5, double(nntest_avx5)));
 #endif
 	}
