da0064e948a4c218f10f00e95772b843fff2b3ed
Robert Edgar <robert@drive5.com>
2026-05-16 14:32:23 -0700

    Fix gcc compile error with overloaded MemBytesToStr

--- src/myutils.h
+++ src/myutils.h
@@ -323,7 +323,6 @@ extern string g_ShortCmdLine;
 
 const char *MemBytesToStr(double Bytes);
 static inline const char *MemBytesToStr(uint64 Bytes) { return MemBytesToStr((double) Bytes); }
-static inline const char *MemBytesToStr(uint32 Bytes) { return MemBytesToStr((double) Bytes); }
 unsigned StrToUint(const char *s);
 unsigned StrToUint_err(const char *s);
 unsigned StrToUint(const string &s);
