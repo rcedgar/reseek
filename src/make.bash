aed6ccf2f5e45a11140d4df6fb4b42fc0d1de037
Robert Edgar <robert@drive5.com>
2026-07-21 07:27:51 -0700

    Default to -march=x86-64-v3 for wider CPU compatibility

--- src/make.bash
+++ src/make.bash
@@ -1,7 +1,7 @@
 mkdir -p ../bin
 
 ./gitver.bash
-python3 ../py/vcxproj_make_e539775.py --openmp --bash 2> make.stderr
+python3 ../py/vcxproj_make_0c8d9d1.py --openmp --x86-64-v3 --bash 2> make.stderr
 rc=$?
 
 echo
