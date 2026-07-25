7e41b8294cca0019966e343e5ee10d9aea1c28f3
Robert Edgar <robert@drive5.com>
2026-07-24 17:16:50 -0700

    daily_git

---
+++ src/make_native.bash
@@ -0,0 +1,24 @@
+mkdir -p ../bin
+
+./gitver.bash
+python3 ../py/vcxproj_make_0c8d9d1.py --openmp --bash 2> make.stderr
+rc=$?
+
+echo
+echo
+echo '=== tail make.stderr ==='
+cat make.stderr \
+	| grep -v "lto-wrapper: warning: using serial compilation" \
+	| grep -v "lto-wrapper: note: see the" \
+	| grep -v " warning: Using .dlopen. in statically linked applications" \
+	| tail
+echo
+echo
+if [ $rc == 0 ] ; then
+	echo SUCCESS
+else
+	echo ERROR
+fi
+echo
+
+ls -lh ../bin/reseek
